232a4a1fc69ddfe4b7c772cabff12d2cb7b23d53
zxxtlz <75642081+zxxtlz@users.noreply.github.com>
2024-10-25 12:45:06 +0300

    Added website

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'selector',
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
